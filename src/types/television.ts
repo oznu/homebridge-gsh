f6ab8c21aac019f0c3b0e939a6a017e742e7ff34
banboobee <qzh00033@nifty.ne.jp>
2022-07-09 10:20:11 +0900

    Added more channel/input command.

--- src/types/television.ts
+++ src/types/television.ts
@@ -10,9 +10,6 @@ export class Television {
       //'action.devices.traits.Toggles',
       //'action.devices.traits.AppSelector',
       //'action.devices.traits.TransportControl',
-      //'action.devices.traits.Volume',
-      //'action.devices.traits.Channel',
-      //'action.devices.traits.InputSelector'
     ];
     let attributes = {
       commandOnlyOnOff: false,	//OnOff
@@ -31,20 +28,30 @@ export class Television {
       attributes.commandOnlyChannels = true;
       attributes.availableChannels = [];
       for (const c of service.extras.channels) {
-	let x = {
+	attributes.availableChannels.push({
 	  key: c.Name,
 	  names: [c.ConfiguredName],
 	  number: `${c.Identifier + 1}`,
-	};
-	attributes.availableChannels.push(x);
+	});
       }
     }
     if (service.extras?.inputs && service.extras.inputs.length > 0) {
       traits.push('action.devices.traits.InputSelector');
-      attributes.commandOnlyInputSelector = true;
-      attributes.availableInputs = [];
+      attributes.commandOnlyInputSelector = false;
+      attributes.orderedInputs = true;
+      attributes.availableInputs = [{
+	key: "_tv",	//dummy for stations
+	names: [
+	  {
+	    lang: "en",
+	    name_synonym: [
+	      "_tv",
+	    ]
+	  }
+	]
+      }];
       for (const c of service.extras.inputs) {
-	let x = {
+	attributes.availableInputs.push({
 	  key: c.Name,
 	  names: [
 	    {
@@ -54,8 +61,7 @@ export class Television {
 	      ]
 	    }
 	  ],
-	};
-	attributes.availableInputs.push(x);
+	});
       }
     }
     //console.log(JSON.stringify(traits));
@@ -101,17 +107,19 @@ export class Television {
     if (c = service.characteristics.find(x => x.type === Characteristic.Mute)) {
       response.isMuted =  c.value ? true : false;
     }
-    // if (c = service.characteristics.find(x => x.type === Characteristic.ActiveIdentifier)) {	// meaningless
-    //   let i;
-    //   console.log(c.value);
-    //   if (i = service.extras.inputs.find(x => x.Identifier === c.value)) {
-    // 	response.currentInput = i.Name;
-    //   }
-    //   // else if (i = service.extras.channels.find(x => x.Identifier === c.value)) {
-    //   // 	response.currentInput =  i.name;
-    //   // }
-    // }
-    // console.log(response);
+    if (c = service.characteristics.find(x => x.type === Characteristic.ActiveIdentifier)) {
+      let i;
+      response.currentInput =  "_tv";
+      if (i = service.extras.inputs.find(x => x.Identifier === c.value)) {
+	response.currentInput =  i.Name;
+      }
+      else {
+	i = service.extras.channels.find(x => x.Identifier === c.value);
+      }
+      const d = service.characteristics.find(x => x.type === Characteristic.Name).value;
+      console.log(`Current input selection of ${d} is ${i.ConfiguredName} and response is ${response.currentInput}.` );
+    }
+    //console.log(response);
     
     return response;
     
@@ -147,9 +155,8 @@ export class Television {
         };
         return { payload };
       }
-      // case ('action.devices.commands.setVolume'): {	// No proper characteristic
-      // 	return { payload: { characteristics: [] } };
-      // }
+   // case ('action.devices.commands.setVolume'): {	// No proper characteristic
+   // }
       case ('action.devices.commands.volumeRelative'): {
 	// Characteristic.VolumeSelector.INCREMENT = 0;
 	// Characteristic.VolumeSelector.DECREMENT = 1;
@@ -163,51 +170,162 @@ export class Television {
         return { payload };
       }
       case ('action.devices.commands.selectChannel'): {
-	let payload = null;
 	let c;
 	if (command.execution[0].params?.channelCode) {
 	  const code = command.execution[0].params.channelCode;
 	  if (c = service.extras.channels.find(x => x.Name === code)) {
-            payload = {
+            const payload = {
               characteristics: [{
 		aid: service.aid,
 		iid: service.characteristics.find(x => x.type === Characteristic.ActiveIdentifier).iid,
 		value: c.Identifier,
               }],
 	    };
+	    service.extras.channels.lastchannel = c.Identifier;
+	    service.characteristics.find(x => x.type === Characteristic.ActiveIdentifier).value = c.Identifier;
+	    return { payload };
 	  }
 	} else if (command.execution[0].params?.channelNumber) {
 	  const number = parseInt(command.execution[0].params.channelNumber) - 1;
 	  if (c = service.extras.channels.find(x => x.Identifier === number)) {
-            payload = {
+            const payload = {
               characteristics: [{
 		aid: service.aid,
 		iid: service.characteristics.find(x => x.type === Characteristic.ActiveIdentifier).iid,
 		value: c.Identifier,
               }],
 	    };
+	    service.extras.channels.lastchannel = c.Identifier;
+	    service.characteristics.find(x => x.type === Characteristic.ActiveIdentifier).value = c.Identifier;
+	    return { payload };
 	  }
+      //} else if (command.execution[0].params?.channelName) {
 	}
+	break;
+      }
+      case ('action.devices.commands.relativeChannel'): {
+	const change = command.execution[0].params?.relativeChannelChange;
+	const n = service.extras.channels.length;
+	let c = service.extras.channels.lastchannel !== undefined ?
+	    service.extras.channels.lastchannel :
+	    service.extras.channels[n - 1].Identifier;
+	const d = service.characteristics.find(x => x.type === Characteristic.Name).value;
+	console.log(`Current channel selection of ${d} is ${c}.` );
+	if (change > 0) {
+	  if (++c > service.extras.channels[n - 1].Identifier)
+	    c = service.extras.channels[0].Identifier;
+	} else if (change < 0) {
+	  if (--c < service.extras.channels[0].Identifier)
+	    c = service.extras.channels[n - 1].Identifier;
+	}
+	console.log(`Next channel selection of ${d} is ${c}.` );
+	const payload = {
+          characteristics: [{
+	    aid: service.aid,
+	    iid: service.characteristics.find(x => x.type === Characteristic.ActiveIdentifier).iid,
+	    value: c,
+          }],
+	};
+	service.extras.channels.lastchannel = c;
+	// force to update to ready next channel operations.
+	service.characteristics.find(x => x.type === Characteristic.ActiveIdentifier).value = c;
+	return { payload };
+      }
+      case ('action.devices.returnChannel'): {
+	let c = service.characteristics.find(x => x.type === Characteristic.ActiveIdentifier).value;
+	const d = service.characteristics.find(x => x.type === Characteristic.Name).value;
+	console.log(`Current channel selection of ${d} is ${c}.` );
+	c = service.extras.channels.lastchannel !== undefined ?
+	  service.extras.channels.lastchannel :
+	  service.extras.channels[0].Identifier;
+	console.log(`Next channel selection of ${d} is ${c}.` );
+	const payload = {
+          characteristics: [{
+	    aid: service.aid,
+	    iid: service.characteristics.find(x => x.type === Characteristic.ActiveIdentifier).iid,
+	    value: c,
+          }],
+	};
+	service.extras.channels.lastchannel = c;
+	// force to update to ready next channel operations.
+	service.characteristics.find(x => x.type === Characteristic.ActiveIdentifier).value = c;
 	return { payload };
       }
       case ('action.devices.commands.SetInput'): {
 	const input = command.execution[0].params?.newInput;
-	let payload = null;
 	let c;
 	if (c = service.extras.inputs.find(x => x.Name === input)) {
-	  payload = {
+	  const payload = {
             characteristics: [{
 	      aid: service.aid,
 	      iid: service.characteristics.find(x => x.type === Characteristic.ActiveIdentifier).iid,
 	      value: parseInt(c.Identifier),
             }],
 	  };
+	  const states = {
+	    currentInput: c.Name,
+	  }
+	  service.characteristics.find(x => x.type === Characteristic.ActiveIdentifier).value = c.Identifier;
+	  return { payload, states };
 	}
-	return { payload };
+	break;
+      }
+      case ('action.devices.commands.NextInput'): {
+	let c = service.characteristics.find(x => x.type === Characteristic.ActiveIdentifier).value;
+	let d = service.characteristics.find(x => x.type === Characteristic.Name).value;
+	console.log(`Current input selection of ${d} is ${c}.` );
+	let n = service.extras.inputs.length;
+	if (service.extras.channels.find(x => x.Identifier === c)) {
+	  c = service.extras.inputs[0].Identifier;
+	} else if (c === service.extras.inputs[n - 1].Identifier) {
+	  c = service.extras.inputs[0].Identifier;
+	} else if (n > 1) {
+	  c++;
+	}
+	console.log(`Next input selection of ${d} is ${c}.` );
+	const payload = {
+          characteristics: [{
+	    aid: service.aid,
+	    iid: service.characteristics.find(x => x.type === Characteristic.ActiveIdentifier).iid,
+	    value: c,
+          }],
+	};
+	const states = {
+	  currentInput: service.extras.inputs.find(x => x.Identifier === c).Name,
+	}
+	// force to update to ready next Next/Previous
+	service.characteristics.find(x => x.type === Characteristic.ActiveIdentifier).value = c;
+	return { payload, states };
+      }
+      case ('action.devices.commands.PreviousInput'): {
+	let c = service.characteristics.find(x => x.type === Characteristic.ActiveIdentifier).value;
+	let d = service.characteristics.find(x => x.type === Characteristic.Name).value;
+	console.log(`Current input selection of ${d} is ${c}.` );
+	let n = service.extras.inputs.length;
+	if (service.extras.channels.find(x => x.Identifier === c)) {
+	  c = service.extras.inputs[n - 1].Identifier;
+	} else if (c === service.extras.inputs[0].Identifier) {
+	  c = service.extras.inputs[n - 1].Identifier;
+	} else if (n > 1) {
+	  c--;
+	}
+	console.log(`Next input selection of ${d} is ${c}.` );
+	const payload = {
+          characteristics: [{
+	    aid: service.aid,
+	    iid: service.characteristics.find(x => x.type === Characteristic.ActiveIdentifier).iid,
+	    value: c,
+          }],
+	};
+	const states = {
+	  currentInput: service.extras.inputs.find(x => x.Identifier === c).Name,
+	}
+	// force to update to ready next Next/Previous
+	service.characteristics.find(x => x.type === Characteristic.ActiveIdentifier).value = c;
+	return { payload, states };
       }
     }
     
     return { payload: null };
   }
-
 }
